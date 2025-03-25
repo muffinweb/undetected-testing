d9a9466bfff43a2f19d2d086b50e4703d8ff37bb
muffinweb <ugurcengiztr35@gmail.com>
2025-03-25 13:26:00 +0300

    remove workflows

--- raw_gitlab_uc.py
+++ raw_gitlab_uc.py
@@ -1,6 +1,6 @@
 from seleniumbase import SB
 
-with SB(uc=True, test=True) as sb:
+with SB(uc=True, test=True, headless=True) as sb:
     url = "https://gitlab.com/users/sign_in"
     sb.uc_open_with_reconnect(url, 5)
     sb.uc_gui_click_captcha()
